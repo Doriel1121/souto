beac6bb1ffff53cf7aca30e45b834fb9b7142f07
omer <omer@run.ai>
2020-10-30 14:13:07 +0200

    Table nameg

--- souto-server/tickets.js
+++ souto-server/tickets.js
@@ -4,7 +4,7 @@ const connection = dal.connection;
 
 exports.getAllTicketsByUserId = (userId, callback) => {
   connection.query(
-    "SELECT * FROM tickets WHERE user_id = ? AND active = 1",
+    "SELECT * FROM Tickets WHERE user_id = ? AND active = 1",
     [userId],
     (error, rows) => {
       if (error) throw error;
