6fa3f64492cb5b684d7cea9cceb09c066e55475a
omer <omer@run.ai>
2020-10-30 21:59:14 +0200

    Align to center

--- souto-app/src/components/ticket.js
+++ souto-app/src/components/ticket.js
@@ -1,22 +1,17 @@
 import React, { Component } from "react";
-import { Typography, Avatar } from "@material-ui/core";
+import { Typography, Avatar, Grid } from "@material-ui/core";
 
 const styles = {
   pickBox: {
     margin: "2.5vh",
-    top: "5vh",
-    width: "90%",
-    height: "8vh",
+    width: "92.5vw",
     borderRadius: "15px",
     backgroundColor: "white",
     boxShadow: "0 0 10px 1px #3e3e3e",
   },
   title: {
     fontSize: "small",
-    position: "relative",
-    top: -7,
     color: "#646261",
-    marginLeft: "2vh",
     display: "inline-block",
     fontWeight: "bolder",
   },
@@ -25,31 +20,44 @@ const styles = {
     height: "5vh",
     backgroundColor: "purple",
     display: "inline-block",
-    transform: "translateY(30%)",
-    marginLeft: "2vh",
   },
   rearInfo: {
     fontSize: "x-small",
     color: "#BFBAB8 ",
-    marginLeft: "9vh",
-    lineHeight: "0.07",
   },
+  textBox: {
+    paddingLeft: "0px"
+  },
+  avatarBox: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'column'
+  }
 };
 
 export default class Ticket extends Component {
   render() {
     return (
-      <div style={styles.pickBox}>
-        <Avatar style={styles.avatar}>
-          <span style={styles.AvatarContent}></span>
-        </Avatar>
-        <Typography style={styles.title}>{this.props.info.title}</Typography>
-        <Typography style={styles.rearInfo}>
-          {this.props.info.description.length > 50
-            ? this.props.info.description.slice(0, 45) + "..."
-            : this.props.info.description}
-        </Typography>
-      </div>
+      <Grid container
+      container
+      direction="row"
+      justify="flex-start"
+      alignItems="start"
+      spacing={3}
+      fil
+      style={styles.pickBox}>
+        <Grid item xs={2} style={styles.avatarBox}>
+          <Avatar style={styles.avatar}>
+            <span style={styles.AvatarContent}></span>
+          </Avatar>
+        </Grid>
+        <Grid item xs={10} style={styles.textBox}>
+          <Typography style={styles.title}>{this.props.info.title}</Typography>
+          <Typography style={styles.rearInfo}>
+            {this.props.info.description}
+          </Typography>
+        </Grid>
+      </Grid>
     );
   }
 }
