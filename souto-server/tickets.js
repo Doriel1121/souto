eb494adafd8f73cd01d140bc2476cf662f14cf27
omer <omer@run.ai>
2020-10-31 13:51:30 +0200

    A

--- souto-server/tickets.js
+++ souto-server/tickets.js
@@ -15,7 +15,7 @@ exports.getAllTicketsByUserId = (userId, callback) => {
 
 exports.updateTicket = (ticketId, ticket, callback) => {
     connection.query("UPDATE Tickets SET title = ?, description = ?, status = ?, last_update_time = ? WHERE id = ?", 
-    [ticket.title, ticket.desciption, ticket.status, (new Date()).toISOString(), ticketId], (error, _) => {
+    [ticket.title, ticket.description, ticket.status, (new Date()).toISOString(), ticketId], (error, _) => {
         if(error) throw error;
         callback()
     })
