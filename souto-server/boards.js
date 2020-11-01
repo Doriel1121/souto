8c5bc950699391d79037ff6c73d9f0e884724eb1
omer <omer@run.ai>
2020-11-01 23:35:10 +0200

    Change column name

--- souto-server/boards.js
+++ souto-server/boards.js
@@ -5,7 +5,7 @@ const connection = dal.connection;
 exports.addBoard = (board, callback) => {
     let key = '1111'
     let secret = '2222'
-    connection.query("INSERT INTO Boards (name, key, secret, creation_time) VALUES (?, ?, ?, ?)", [board.name, key, secret, (new Date()).toISOString()], (error) => {
+    connection.query("INSERT INTO Boards (name, public_key, secret, creation_time) VALUES (?, ?, ?, ?)", [board.name, key, secret, (new Date()).toISOString()], (error) => {
         if(error) throw error;
         connection.query("SELECT * FROM Boards WHERE name = ? ORDER BY creation_time DESC", [board.name], (error, rows) => {
             if(error) throw error;
