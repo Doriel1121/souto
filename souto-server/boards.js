68880369767216295251455744b0f5af4762eedc
omer <omer@run.ai>
2020-11-01 23:28:51 +0200

    Change query to string

--- souto-server/boards.js
+++ souto-server/boards.js
@@ -3,8 +3,8 @@ const dal = require("./data_access_core");
 const connection = dal.connection;
 
 exports.addBoard = (board, callback) => {
-    let key = 1111
-    let secret = 2222
+    let key = '1111'
+    let secret = '2222'
     connection.query("INSERT INTO Boards (name, key, secret, creation_time) VALUES (?, ?, ?, ?)", [board.name, key, secret, (new Date()).toISOString()], (error) => {
         if(error) throw error;
         connection.query("SELECT * FROM Boards WHERE name = ? ORDER BY creation_time DESC", [board.name], (error, rows) => {
