3ced5abdd0303aa396597a58cb764721e5f828f9
Doriel aboya <doriel494@gmail.com>
2020-11-01 16:33:23 +0200

    new column

--- souto-server/main.js
+++ souto-server/main.js
@@ -5,18 +5,20 @@ const tickets = require("./tickets");
 var https = require("https");
 var fs = require("fs");
 
-var httpsOptions = {
-  key: fs.readFileSync("/home/ubuntu/key.pem"),
-  cert: fs.readFileSync("/home/ubuntu/cert.pem"),
-};
+// var httpsOptions = {
+//   key: fs.readFileSync("/home/ubuntu/key.pem"),
+//   cert: fs.readFileSync("/home/ubuntu/cert.pem"),
+// };
 
 const app = express();
 const port = 3002;
 
-https.createServer(httpsOptions, app).listen(port, () => {
+// https.createServer(httpsOptions, app).listen(port, () => {
+//   console.log(`Server started at port ${port}`);
+// });
+app.listen(port, () => {
   console.log(`Server started at port ${port}`);
 });
-
 app
   .use(bodyParser.json())
   .use(cors())
@@ -53,6 +55,7 @@ app
   .post("/board/tickets/add/:boardId", (request, response) => {
     console.log("New ticket request")
     tickets.addTicket(request.params.boardId, request.body, (ticket) => {
+      console.log(ticket);
       response.send(ticket)
     })
   });
--- souto-server/tickets.js
+++ souto-server/tickets.js
@@ -14,51 +14,51 @@ exports.getAllTicketsByUserId = (userId, callback) => {
 };
 
 exports.updateTicket = (ticketId, ticket, callback) => {
-    connection.query("UPDATE Tickets SET title = ?, description = ? WHERE id = ?", 
-    [ticket.title, ticket.description, ticketId], (error, _) => {
-        if(error) throw error;
-        callback()
+  connection.query("UPDATE Tickets SET title = ?, description = ? , icon = ? WHERE id = ?",
+    [ticket.title, ticket.description, ticket.icon, ticketId], (error, _) => {
+      if (error) throw error;
+      callback()
     })
 }
 
 exports.updateStatus = (id, status, callback) => {
   connection.query("UPDATE UserTicketMigration SET status = ? WHERE id = ?", [status, id], (error) => {
-    if(error) throw error;
+    if (error) throw error;
     callback()
   })
 }
 
 exports.deleteTicket = (ticketId, callback) => {
   connection.query("UPDATE Tickets SET active = 0 WHERE id = ?", [ticketId], (error) => {
-    if(error) throw error;
+    if (error) throw error;
     callback();
   })
 }
 
 exports.getAllTicketByBoardId = (boardId, callback) => {
   connection.query("SELECT * from Tickets WHERE board_id = ? AND active = 1", [boardId], (error, rows) => {
-    if(error) throw error;
+    if (error) throw error;
     callback(rows)
   })
 }
 
 exports.addTicket = (boardId, ticket, callback) => {
-  connection.query("INSERT INTO Tickets (board_id, title, description, creation_time, active) VALUES (?, ?, ?, ?, 1)", 
-  [boardId, ticket.title, ticket.description, (new Date()).toISOString()], (error) => {
-    if(error) throw error;
-    connection.query("SELECT * FROM Tickets WHERE board_id = ? AND title = ? ORDER BY creation_time DESC", [boardId, ticket.title], (error, rows) => {
+  connection.query("INSERT INTO Tickets (board_id, title, description, creation_time, active , icon) VALUES (?, ?, ?, ?, 1,?)",
+    [boardId, ticket.title, ticket.description, (new Date()).toISOString(), ticket.icon], (error) => {
       if (error) throw error;
-      let newTicket = rows[0]
-      connection.query("SELECT * FROM Users WHERE board_id = ?", [boardId], (error, registeredUser) => {
+      connection.query("SELECT * FROM Tickets WHERE board_id = ? AND title = ? ORDER BY creation_time DESC", [boardId, ticket.title], (error, rows) => {
         if (error) throw error;
-        let userRows = registeredUser.map(usr => {
-          return [usr.id, newTicket.id, "TODO", (new Date()).toISOString()]
-        })
-        connection.query("INSERT INTO UserTicketMigration (user_id, ticket_id, status, last_update) VALUES ?", [userRows], (error) => {
-          if(error) throw error;
-          callback(newTicket)
+        let newTicket = rows[0]
+        connection.query("SELECT * FROM Users WHERE board_id = ?", [boardId], (error, registeredUser) => {
+          if (error) throw error;
+          let userRows = registeredUser.map(usr => {
+            return [usr.id, newTicket.id, "TODO", (new Date()).toISOString()]
+          })
+          connection.query("INSERT INTO UserTicketMigration (user_id, ticket_id, status, last_update) VALUES ?", [userRows], (error) => {
+            if (error) throw error;
+            callback(newTicket)
+          })
         })
       })
     })
-  })
 }
