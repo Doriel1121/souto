849f62cc70e38a18b556623e0754a682f1522250
omer <omer@run.ai>
2020-11-02 00:09:10 +0200

    Add generated key

--- souto-server/boards.js
+++ souto-server/boards.js
@@ -3,8 +3,8 @@ const dal = require("./data_access_core");
 const connection = dal.connection;
 
 exports.addBoard = (board, callback) => {
-    let key = '1111'
-    let secret = '2222'
+    let key = genetateKey()
+    let secret = genetateKey()
     connection.query("INSERT INTO Boards (name, public_key, secret, creation_time) VALUES (?, ?, ?, ?)", [board.name, key, secret, (new Date()).toISOString()], (error) => {
         if(error) throw error;
         connection.query("SELECT * FROM Boards WHERE name = ? ORDER BY creation_time DESC", [board.name], (error, rows) => {
@@ -12,4 +12,8 @@ exports.addBoard = (board, callback) => {
             callback(rows[0])
         })
     })
+}
+
+let genetateKey = () => {
+    return (Math.floor(Math.random() * 8999) + 1000).toString()
 }
