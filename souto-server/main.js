6ddcf0396bf25685f446f04f7eba85726425afd5
omer <omer@run.ai>
2020-11-02 23:42:59 +0200

    add last

--- souto-server/main.js
+++ souto-server/main.js
@@ -1,82 +1,92 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const tickets = require("./tickets");
-const users = require("./users");
-const boards = require("./boards");
-var https = require("https");
-var fs = require("fs");
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const tickets = require('./tickets')
+const users = require('./users')
+const boards = require('./boards')
+var https = require('https')
+var fs = require('fs')
 
 var httpsOptions = {
-  key: fs.readFileSync("/home/ubuntu/key.pem"),
-  cert: fs.readFileSync("/home/ubuntu/cert.pem"),
-};
+  key: fs.readFileSync('/home/ubuntu/key.pem'),
+  cert: fs.readFileSync('/home/ubuntu/cert.pem'),
+}
 
-const app = express();
-const port = 3002;
+const app = express()
+const port = 3002
 
 https.createServer(httpsOptions, app).listen(port, () => {
-  console.log(`Server started at port ${port}`);
-});
+  console.log(`Server started at port ${port}`)
+})
 
 app
   .use(bodyParser.json())
   .use(cors())
-  .get("/tickets/:userId", (request, response) => {
-    console.log("New tickets request arrived");
+  .get('/tickets/:userId', (request, response) => {
+    console.log('New tickets request arrived')
     tickets.getAllTicketsByUserId(request.params.userId, (tickets) => {
       response.send(tickets)
     })
   })
-  .post("/tickets/update", (request, response) => {
-    console.log("Change ticket request")
+  .post('/tickets/update', (request, response) => {
+    console.log('Change ticket request')
     tickets.updateStatus(request.body.id, request.body.status, () => {
       response.sendStatus(200)
     })
   })
-  .post("/tickets/delete/:id", (request, response) => {
-    console.log("Delete ticket request")
+  .post('/tickets/delete/:id', (request, response) => {
+    console.log('Delete ticket request')
     tickets.deleteTicket(request.params.id, () => {
       response.sendStatus(200)
     })
   })
-  .get("/board/tickets/:boardId", (request, response) => {
-    console.log("Manager ticket request")
+  .get('/board/tickets/:boardId', (request, response) => {
+    console.log('Manager ticket request')
     tickets.getAllTicketByBoardId(request.params.boardId, (tickets) => {
       response.send(tickets)
     })
   })
-  .post("/board/tickets/update", (request, response) => {
-    console.log("Change ticket request")
+  .post('/board/tickets/update', (request, response) => {
+    console.log('Change ticket request')
     tickets.updateTicket(request.body.id, request.body, () => {
       response.sendStatus(200)
     })
   })
-  .post("/board/tickets/add/:boardId", (request, response) => {
-    console.log("New ticket request")
+  .post('/board/tickets/add/:boardId', (request, response) => {
+    console.log('New ticket request')
     tickets.addTicket(request.params.boardId, request.body, (ticket) => {
       response.send(ticket)
     })
   })
-  .post("/boards/add", (request, response) => {
-    console.log("New board request")
+  .post('/boards/add', (request, response) => {
+    console.log('New board request')
     boards.addBoard(request.body, (board) => {
       response.send(board)
     })
   })
-  .post("/users/add/:boardId", (request, response) => {
-    console.log("New register user request")
+  .post('/users/add/:boardId', (request, response) => {
+    console.log('New register user request')
     users.addUser(request.params.boardId, request.body, (user) => {
       response.send(user)
     })
   })
-  .get("/board/key/:boardKey", (request, response) => {
-    console.log("Board by key request")
+  .get('/board/key/:boardKey', (request, response) => {
+    console.log('Board by key request')
     boards.getBoardByKey(request.params.boardKey, (board) => {
-      if(board !== undefined) {
+      if (board !== undefined) {
         response.send(board)
-        return;
+        return
       }
       response.sendStatus(500)
     })
-  });
+  })
+  .get('/board/secret/:boardSecret', (request, response) => {
+    console.log('Board by secret request')
+    boards.getBoardBySecret(request.params.boardSecret, (board) => {
+      if (board !== undefined) {
+        response.send(board)
+        return
+      }
+      response.sendStatus(500)
+    })
+  })
