b4d2ece5eb0028349dd442d11f6a5630d2d61a81
omer <omer@run.ai>
2020-11-02 23:40:21 +0200

    Add secret get

--- souto-server/boards.js
+++ souto-server/boards.js
@@ -1,30 +1,57 @@
-const dal = require("./data_access_core");
+const dal = require('./data_access_core')
 
-const connection = dal.connection;
+const connection = dal.connection
 
 exports.addBoard = (board, callback) => {
-    let key = genetateKey()
-    let secret = genetateKey()
-    connection.query("INSERT INTO Boards (name, public_key, secret, creation_time) VALUES (?, ?, ?, ?)", [board.name, key, secret, (new Date()).toISOString()], (error) => {
-        if(error) throw error;
-        connection.query("SELECT * FROM Boards WHERE name = ? ORDER BY creation_time DESC", [board.name], (error, rows) => {
-            if(error) throw error;
-            callback(rows[0])
-        })
-    })
+  let key = genetateKey()
+  let secret = genetateKey()
+  connection.query(
+    'INSERT INTO Boards (name, public_key, secret, creation_time) VALUES (?, ?, ?, ?)',
+    [board.name, key, secret, new Date().toISOString()],
+    (error) => {
+      if (error) throw error
+      connection.query(
+        'SELECT * FROM Boards WHERE name = ? ORDER BY creation_time DESC',
+        [board.name],
+        (error, rows) => {
+          if (error) throw error
+          callback(rows[0])
+        }
+      )
+    }
+  )
 }
 
 exports.getBoardByKey = (boardKey, callback) => {
-    connection.query("SELECT * FROM Boards where public_key = ?", [boardKey], (error, rows) => {
-        if(error) throw error;
-        if (rows.length > 0) {
-            callback(rows[0])
-        } else {
-            callback(undefined)
-        }
-    })
+  connection.query(
+    'SELECT * FROM Boards where public_key = ?',
+    [boardKey],
+    (error, rows) => {
+      if (error) throw error
+      if (rows.length > 0) {
+        callback(rows[0])
+      } else {
+        callback(undefined)
+      }
+    }
+  )
+}
+
+exports.getBoardBySecret = (boardSecret, callback) => {
+  connection.query(
+    'SELECT * FROM Boards where secret = ?',
+    [boardSecret],
+    (error, rows) => {
+      if (error) throw error
+      if (rows.length > 0) {
+        callback(rows[0])
+      } else {
+        callback(undefined)
+      }
+    }
+  )
 }
 
 let genetateKey = () => {
-    return (Math.floor(Math.random() * 8999) + 1000).toString()
+  return (Math.floor(Math.random() * 8999) + 1000).toString()
 }
