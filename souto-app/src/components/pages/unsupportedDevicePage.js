1c32c869f4eb115b8a05dec0703cf55db1651ca4
omer <omer@run.ai>
2020-11-05 20:39:20 +0200

    Unsupport

---
+++ souto-app/src/components/pages/unsupportedDevicePage.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import MobileImage from '../../resources/warning-mobile-device.png'
+
+const styles = {
+  page: {
+    height: '100vh',
+    background: 'linear-gradient(45deg, #00B4DB, #1C6F8C)',
+    textAlign: 'center',
+    padding: '10vh',
+    color: 'white',
+  },
+  title: {
+    fontSize: '10vh',
+  },
+  message: {
+    fontSize: '4vh',
+  },
+}
+
+export default class UnsupportedDevicePage extends Component {
+  render() {
+    return (
+      <div style={styles.page}>
+        <div style={styles.title}>Unsupported device</div>
+        <div style={styles.imageContainer}>
+          <img alt="mobile device" src={MobileImage} />
+        </div>
+        <div style={styles.message}>
+          Make sure the application runs on a mobile device or an emulator
+        </div>
+      </div>
+    )
+  }
+}
