7cf9d3bcab463ae258264018a77c9513cac34ede
omer <omer@run.ai>
2020-11-09 18:18:55 +0200

    Revert "Merge pull request #15 from omer-dayan/anotherserverholder"
    This reverts commit d9794c1a3c17812b3c6549e64490f4fadd96b95f, reversing
    changes made to 7ea4aa687e20b96cf46648fe6ff9f05ff433c2eb.

--- souto-app/src/ManagerApp.js
+++ souto-app/src/ManagerApp.js
@@ -2,7 +2,6 @@ import { Switch, HashRouter as Router, Route } from 'react-router-dom'
 import React, { Component } from 'react'
 import { ManagerAppDataProvider } from './managerAppDataProvider'
 import ManagerPage from './components/pages/managerPage'
-import ManageUsersPage from './components/pages/ManageUsersPage'
 
 export default class ManagerApp extends Component {
   render() {
@@ -12,9 +11,6 @@ export default class ManagerApp extends Component {
           <Switch>
             <Route exact path="/manager/main">
               <ManagerPage />
-            </Route>
-            <Route exact path ="/manager/userProgress">
-              <ManageUsersPage/>
             </Route>
           </Switch>
         </Router>
