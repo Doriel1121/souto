56a360f632f498e5d69f6d61c5c6f69478e6978b
omer <omer@run.ai>
2020-11-10 15:24:38 +0200

    Link

---
+++ souto-app/src/components/linkValidator.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react'
+import {
+  Backdrop,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+} from '@material-ui/core'
+import ErrorIcon from '@material-ui/icons/Error'
+
+const styles = {
+  backdrop: {
+    zIndex: 2,
+    color: '#fff',
+  },
+  mainText: {
+    textAlign: 'center',
+  },
+  warningSign: { fontSize: 100, color: '#444444' },
+}
+
+export default class LinkValidator extends Component {
+  renderValidation = () => {
+    return (
+      <Backdrop style={styles.backdrop} open={true}>
+        <Card>
+          <CardContent>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              spacing={0}
+            >
+              <Grid item xs={12}>
+                <ErrorIcon style={styles.warningSign} />
+              </Grid>
+              <Grid item xs={12}>
+                Are you sure you want to continue?
+              </Grid>
+            </Grid>
+          </CardContent>
+          <CardActions>
+            <Grid
+              container
+              direction="row"
+              justify="flex-start"
+              alignItems="center"
+              spacing={3}
+            >
+              <Grid item xs={4}>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="secondary"
+                  onClick={(e) => {
+                    if (this.props.action !== undefined) this.props.action(e)
+                    this.props.close()
+                  }}
+                >
+                  Continue
+                </Button>
+              </Grid>
+              <Grid item xs={4}></Grid>
+              <Grid item xs={4}>
+                <Button
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  onClick={(e) => {
+                    if (this.props.cancel !== undefined) this.props.cancel(e)
+                    this.props.close()
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Grid>
+            </Grid>
+          </CardActions>
+        </Card>
+      </Backdrop>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.open ? this.renderValidation() : this.props.children}
+      </div>
+    )
+  }
+}
