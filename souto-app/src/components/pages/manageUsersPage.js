7ebbfcc4ed9e11636f29d4e594a8120602bd5534
Doriel aboya <doriel494@gmail.com>
2020-11-12 09:42:48 +0200

    fix percent

--- souto-app/src/components/pages/manageUsersPage.js
+++ souto-app/src/components/pages/manageUsersPage.js
@@ -1,40 +1,40 @@
-import axios from 'axios'
-import React, { Component } from 'react'
-import Menu from '../menu'
-import config from '../../config'
-import CircularProgress from '@material-ui/core/CircularProgress'
-import Backdrop from '@material-ui/core/Backdrop'
-import { MobileStepper, Grid } from '@material-ui/core'
+import axios from "axios"
+import React, { Component } from "react"
+import Menu from "../menu"
+import config from "../../config"
+import CircularProgress from "@material-ui/core/CircularProgress"
+import Backdrop from "@material-ui/core/Backdrop"
+import { MobileStepper, Grid } from "@material-ui/core"
 
 const styles = {
   progressBar: {
-    flexGrow: '1',
-    backgroundColor: 'transparent',
-    paddingTop: '3vh',
-    marginRight: '0',
+    flexGrow: "1",
+    backgroundColor: "transparent",
+    paddingTop: "3vh",
+    marginRight: "0",
   },
   names: {
-    textAlign: 'center',
-    transform: 'translateY(1.7vh)',
+    textAlign: "center",
+    transform: "translateY(1.7vh)",
   },
   userProgressInfo: {
-    marginTop: '5vh',
+    marginTop: "5vh",
   },
   percent: {
-    transform: 'translateY(1.7vh)',
+    transform: "translateY(1.7vh)",
   },
   svgLoading: {
-    display: 'block',
-    width: '10vw',
-    height: '10vh',
-    margin: 'auto',
+    display: "block",
+    width: "10vw",
+    height: "10vh",
+    margin: "auto",
   },
   empty: {
-    textAlign: 'center',
+    textAlign: "center",
   },
   backdrop: {
     zIndex: 1,
-    color: '#fff',
+    color: "#fff",
   },
 }
 
@@ -52,15 +52,15 @@ export default class ManageUsersPage extends Component {
     axios
       .get(
         config.server +
-          '/board/allusers/' +
-          window.localStorage.getItem('captainBoardId')
+          "/board/allusers/" +
+          window.localStorage.getItem("captainBoardId")
       )
       .then((res) => {
         this.setState({ usersProgress: res.data, open: false })
       })
       .catch((err) => {
         console.log(err)
-        alert('Sorry could not get the data please try again later ')
+        alert("Sorry could not get the data please try again later ")
       })
   }
 
@@ -74,7 +74,8 @@ export default class ManageUsersPage extends Component {
           </Backdrop>
           {this.state.usersProgress.length > 0 ? (
             this.state.usersProgress.map((element, index) => {
-              let percent = 100 / element.c + 1 * element.o
+              let numerator = 100 / element.c + 1
+              numerator = numerator * element.o
               return (
                 <React.Fragment key={index}>
                   <Grid style={styles.names} item xs={3}>
@@ -90,7 +91,7 @@ export default class ManageUsersPage extends Component {
                     />
                   </Grid>
                   <Grid style={styles.percent} item xs={2}>
-                    {percent.toFixed(0)}%
+                    {numerator.toFixed(0)}%
                   </Grid>
                 </React.Fragment>
               )
