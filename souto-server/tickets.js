afc234e578a2fbebbbd1bfb5adbd9391c3a2f9bf
omer <omer@run.ai>
2020-11-13 13:41:44 +0200

    Makwi it kiw

--- souto-server/tickets.js
+++ souto-server/tickets.js
@@ -120,10 +120,14 @@ exports.updateFlag = (userTicketId, value, callback) => {
   )
 }
 
-exports.getFlaggedTicketsByUserId = (userId , callback) => {
-  console.log("in");
-  connection.query('SELECT Tickets.id , Tickets.board_id , Tickets.title , Tickets.description , Tickets.active , Tickets.icon FROM Tickets JOIN UserTicketMigration ON Tickets.id = UserTicketMigration.ticket_id WHERE UserTicketMigration.user_id = ? AND UserTicketMigration.flag = "1"' , [userId] , (error , rows) => {
-    if (error) throw error
-    callback(rows)
-  })
+exports.getFlaggedTicketsByUserId = (userId, callback) => {
+  console.log('in')
+  connection.query(
+    'SELECT Tickets.id , Tickets.board_id , Tickets.title , Tickets.description , Tickets.active , Tickets.icon FROM Tickets JOIN UserTicketMigration ON Tickets.id = UserTicketMigration.ticket_id WHERE UserTicketMigration.user_id = ? AND UserTicketMigration.flag = "1" AND Tickets.active = 1',
+    [userId],
+    (error, rows) => {
+      if (error) throw error
+      callback(rows)
+    }
+  )
 }
