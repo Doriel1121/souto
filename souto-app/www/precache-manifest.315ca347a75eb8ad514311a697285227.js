28f5e91fe3cc36f774cab3eaa0610dd1130391d3
omer dayan <omerxdayan1@gmail.com>
2020-11-16 14:51:25 +0200

    Add cordova

---
+++ souto-app/www/precache-manifest.315ca347a75eb8ad514311a697285227.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc747e775b8513cbed63c21201c204db",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1872fd01e89756132d4b",
+    "url": "./static/css/main.dc14e679.chunk.css"
+  },
+  {
+    "revision": "aa85e81f298daf8bc12f",
+    "url": "./static/js/2.f39a27a7.chunk.js"
+  },
+  {
+    "revision": "67360280b1e728771bc49dea7ac92e3c",
+    "url": "./static/js/2.f39a27a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1872fd01e89756132d4b",
+    "url": "./static/js/main.75cbc895.chunk.js"
+  },
+  {
+    "revision": "2a4408debaed68a3b0fa",
+    "url": "./static/js/runtime-main.15983474.js"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "./static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "18e8b5cd344854c8c86d49aeff0cc950",
+    "url": "./static/media/board-empty.18e8b5cd.png"
+  },
+  {
+    "revision": "58d19d8e5d0cb34083431afcfb0b1324",
+    "url": "./static/media/icon1.58d19d8e.png"
+  },
+  {
+    "revision": "b069c9dae21b8547553c995ecbabc15e",
+    "url": "./static/media/icon2.b069c9da.png"
+  },
+  {
+    "revision": "54f41e0b0aa7c7dc32bfefe012c2e9b4",
+    "url": "./static/media/login-image-0.54f41e0b.png"
+  },
+  {
+    "revision": "c94a9297b6147158d6e3703629cccbc8",
+    "url": "./static/media/logo-color.c94a9297.png"
+  },
+  {
+    "revision": "5ae20ba50f0f8bb40e6ca256a1a7a967",
+    "url": "./static/media/logo-white.5ae20ba5.png"
+  },
+  {
+    "revision": "3cffa9983f8f4665612228dc29db955e",
+    "url": "./static/media/logo.3cffa998.png"
+  },
+  {
+    "revision": "8b4ebcf11351d8709d1501135fa3bb50",
+    "url": "./static/media/onBoardCaptainKey.8b4ebcf1.png"
+  },
+  {
+    "revision": "9a28c6d971c94ab25cd21d8cfdac6b2e",
+    "url": "./static/media/onBoardFlag.9a28c6d9.png"
+  },
+  {
+    "revision": "69a3cce996eefa4f4f5492c196301e3e",
+    "url": "./static/media/onBoardSailorKey.69a3cce9.png"
+  },
+  {
+    "revision": "be55843837f85f56edbbd52c2c542a6a",
+    "url": "./static/media/onBoardWelcome.be558438.png"
+  },
+  {
+    "revision": "b4229a318597d4e42caeb15689fa0fe7",
+    "url": "./static/media/onBoardingDrag.b4229a31.gif"
+  },
+  {
+    "revision": "77219dca98e22153fb4168b114f3b5d8",
+    "url": "./static/media/red-flag.77219dca.png"
+  },
+  {
+    "revision": "446e4535d6f7a2d6a1fa23d4867e994b",
+    "url": "./static/media/warning-mobile-device.446e4535.png"
+  },
+  {
+    "revision": "cd7d616bb571cd4df50401c50fd0e43e",
+    "url": "./static/media/white-flag.cd7d616b.png"
+  }
+]);
